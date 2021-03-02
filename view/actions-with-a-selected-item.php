b4645dca8ad6cdff49ad5ca2776c6d409aba4dac
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2021-03-03 00:11:11 +0100

    Some refactoring.

--- view/actions-with-a-selected-item.php
+++ view/actions-with-a-selected-item.php
@@ -12,11 +12,15 @@ if(isset($_GET['select-id']) and !empty($_GET['select-id']) and is_numeric($_GET
 
 ?>
 <div class="tsw-actions-zone">
-    <h3>Selected <span class="tsw-actions-zone-site-home-url"><?= $current_selected_url['home_url'] ?></span></h3>
-    <input type="hidden" id="tsw-current-selected-site-id" value="<?= $current_selected_url['id'] ?>">
+    <h3>Selected <span class="tsw-actions-zone-site-home_url"><?= $current_selected_url['home_url'] ?></span></h3>
+    <!-- --> 
+    <input type="hidden" id="tsw-current-selected-id" value="<?= $current_selected_url['id'] ?>">
     <input type="hidden" id="tsw-current-selected-max_depth_allowed" value="<?= $current_selected_url['max_depth_allowed'] ?>">
     <input type="hidden" id="tsw-current-selected-max_urls_allowed" value="<?= $current_selected_url['max_urls_allowed'] ?>">
     <input type="hidden" id="tsw-current-selected-max_secs_allowed" value="<?= $current_selected_url['max_secs_allowed'] ?>">
+    <input type="hidden" id="tsw-current-selected-crawl_type" value="<?= $current_selected_url['crawl_type'] ?>">
+    <input type="hidden" id="tsw-current-selected-web_ping_enabled" value="<?= $current_selected_url['web_ping_enabled'] ?>">
+    <!-- --> 
     <button type="button" class="tsw-btn-see-results">See results</button>
     <button type="button" name="tsw-btn-study-site" id="tsw-btn-study-site" class="tsw-btn-study-site">Study Site</button>
     <span id="tsw-box-study-site-status">Standby</span>
